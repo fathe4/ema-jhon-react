0c032f0980fd4e2acbc02bbde33c73f5947a574f
unknown <freelancerfathe@gmail.com>
2021-10-28 00:23:52 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,18 @@
+console.log(process.env);
+const firebaseConfig = {
+    // apiKey: process.env.REACT_APP_API_KEY,
+    // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    // projectId: process.env.REACT_APP_PROJECT_ID,
+    // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    // appId: process.env.REACT_APP_APP_ID,
+    apiKey: "AIzaSyAQgmuyuHUdnCYIMchIxvtmuro1lDAZusk",
+    authDomain: "private-route-6d709.firebaseapp.com",
+    projectId: "private-route-6d709",
+    storageBucket: "private-route-6d709.appspot.com",
+    messagingSenderId: "170161447695",
+    appId: "1:170161447695:web:1f69b780edb43d288a547a",
+    measurementId: "G-90JY43D67T"
+};
+
+export default firebaseConfig;
